285e505b004a6865a402097b6f2c0221fe6d7b4b
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2018-12-31 16:32:46 -0200

    create first mutation

--- dist/graphql/schema.js
+++ dist/graphql/schema.js
@@ -23,10 +23,21 @@ const typeDefs = `
   type Query {
     allUsers: [ User! ]!
   }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
+  }
 `;
 const resolvers = {
     Query: {
         allUsers: () => usersMock
+    },
+    Mutation: {
+        createUser: (parent, args) => {
+            const newUser = Object.assign({ id: usersMock.length + 1 }, args);
+            usersMock.push(newUser);
+            return newUser;
+        }
     }
 };
 exports.default = graphql_tools_1.makeExecutableSchema({ typeDefs, resolvers });
